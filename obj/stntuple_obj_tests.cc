0733a8ce960befd180d70ce5f69dda74de023567
Pavel Murat <murat@fnal.gov>
2015-12-11 13:28:00 -0600

    new

---
+++ obj/stntuple_obj_tests.cc
@@ -0,0 +1,12 @@
+//
+
+#include "obj/stntuple_obj_tests.hh"
+
+//-----------------------------------------------------------------------------
+void test_001_TDisk(double      RMin       , 
+		    double      RMax       , 
+		    double      HexSize    , 
+		    double      MinFraction, 
+		    const char* Opt        ) {
+}
+
