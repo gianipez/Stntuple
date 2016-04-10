27937ab96f3329744b0fdec93aaef47a3173f60d
Pavel Murat <murat@fnal.gov>
2016-04-10 12:31:37 -0500

    - update EventFilter module

--- mod/EventFilter_module.cc
+++ mod/EventFilter_module.cc
@@ -1,7 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+// Filter events passing only the ones from the provided event list
 //
-//  Filter events passing only the ones from the list (A module to look at the provenance of a product.
-//
-//
+// event list contains 3 numbers per event : run, subrun, event
+// if the event list is empty, all events are passed through
+// set _printFreq to get the diagnostics printout
+
 
 #include "MCDataProducts/inc/GenParticleCollection.hh"
 #include "MCDataProducts/inc/SimParticleCollection.hh"
@@ -24,8 +27,10 @@ namespace mu2e {
   class EventFilter : public art::EDFilter {
   protected: 
     vector<int>       _eventList;  // 3 numbers per event: [run, subrun, event]
-    int               _nevents;
+    int               _printFreq;
 
+    int               _nevents;
+    int               _npassed;
   public:
     explicit EventFilter(fhicl::ParameterSet const& pset);
 
@@ -39,33 +44,52 @@ namespace mu2e {
 
   EventFilter::EventFilter(fhicl::ParameterSet const& pset)
     : art::EDFilter(),
-      _eventList(pset.get<vector<int>>("eventList"))
+      _eventList(pset.get<vector<int>>("eventList")),
+      _printFreq(pset.get<int>        ("printFreq"))
   {
     _nevents = _eventList.size()/3;
+    _npassed = 0;
   }
 
 
 //-----------------------------------------------------------------------------
   bool EventFilter::filter(art::Event&  anEvent) {
 
-    uint   eventNumber, runNumber, subRunNumber;
-    bool   found(true);
+    const char oname [] = "EventFilter::filter";
+
+    uint   eventNumber, runNumber, subRunNumber, rn, srn, evn;
+    int    found(1);
+
+    rn  = anEvent.run();
+    srn = anEvent.subRun();
+    evn = anEvent.event();
+
+    if ((evn % _printFreq) == 0) {
+      printf("[%s] : run, subrun event: %8i %8i %8i\n",oname,rn, srn,evn);
+    }
 
     if (_nevents > 0) {
-      found = false;
+      found = 0;
 
       for (int i=0; i<_nevents; i++) {
 	runNumber    = _eventList[3*i+0];
-	eventNumber  = _eventList[3*i+1];
-	subRunNumber = _eventList[3*i+2];
+	subRunNumber = _eventList[3*i+1];
+	eventNumber  = _eventList[3*i+2];
 	
-	if ((anEvent.event () == eventNumber ) && 
-	    (anEvent.run   () == runNumber   ) && 
-	    (anEvent.subRun() == subRunNumber)    ) {
-	  found = true;
+	if ((evn == eventNumber) && (rn == runNumber) && (srn == subRunNumber)) {
+	  found = 1;
+	  break;
 	}
       }
     }
+
+    if (found) _npassed += 1;
+
+    if ((evn % _printFreq) == 0) {
+      printf("[%s] : run, subrun event, found, n(passed): %8i %8i %8i %2i %10i\n",
+	     oname,rn,srn,evn,found,_npassed);
+    }
+
 
     return found;
   } 
