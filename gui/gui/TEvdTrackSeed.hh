1d6d6806ea8d9890baf47316bce9b5dd7e6cb72b
Pavel Murat <murat@fnal.gov>
2016-04-30 20:36:52 -0500

    - provision for track seed display

---
+++ gui/gui/TEvdTrackSeed.hh
@@ -0,0 +1,72 @@
+///////////////////////////////////////////////////////////////////////////////
+// vis node displays one wedge
+///////////////////////////////////////////////////////////////////////////////
+#ifndef TEvdTrackSeed_hh
+#define TEvdTrackSeed_hh
+
+#include "Gtypes.h"
+#include "TClonesArray.h"
+#include "TH1.h"
+#include "TPad.h"
+#include "TArc.h"
+#include "TEllipse.h"
+
+class KalRep;
+class TEvdTrkStrawHit;
+
+class TEvdTrackSeed: public TObject {
+public:
+  
+protected:
+  int                fNumber;
+  const KalRep*      fKrep;
+
+  TObjArray*         fListOfHits;
+
+  static TEllipse*   fgEllipse;
+public:
+//-----------------------------------------------------------------------------
+// constructors and destructor
+//-----------------------------------------------------------------------------
+  TEvdTrackSeed();
+
+  TEvdTrackSeed(int Number, const KalRep* Krep);
+
+  virtual ~TEvdTrackSeed();
+//-----------------------------------------------------------------------------
+// accessors
+//-----------------------------------------------------------------------------
+  int NHits() { return fListOfHits->GetEntriesFast(); }
+
+  TEvdTrkStrawHit* Hit(int I) { 
+    return (TEvdTrkStrawHit*) fListOfHits->UncheckedAt(I); 
+  }
+//-----------------------------------------------------------------------------
+// modifiers
+//-----------------------------------------------------------------------------
+  void AddHit(TObject* Hit) { fListOfHits->Add(Hit); }
+//-----------------------------------------------------------------------------
+// drawing functions
+//-----------------------------------------------------------------------------
+  virtual void  PaintXY  (Option_t* option = "");
+  virtual void  PaintRZ  (Option_t* option = "");
+  virtual void  PaintCal (Option_t* option = "");
+
+  //  virtual void  ExecuteEvent(Int_t event, Int_t px, Int_t py);
+
+  virtual Int_t DistancetoPrimitive   (Int_t px, Int_t py);
+  virtual Int_t DistancetoPrimitiveXY (Int_t px, Int_t py);
+  virtual Int_t DistancetoPrimitiveRZ (Int_t px, Int_t py);
+  virtual Int_t DistancetoPrimitiveCal(Int_t px, Int_t py);
+//-----------------------------------------------------------------------------
+// overloaded methods of TObject
+//-----------------------------------------------------------------------------
+  virtual void  Paint(Option_t* Opt = "");
+  virtual void  Clear(Option_t* Opt = "");
+  virtual void  Print(Option_t* Opt = "") const ; // **MENU**
+
+  ClassDef(TEvdTrackSeed,0)
+};
+
+
+#endif
