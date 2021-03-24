aa9f488e4f2022bae1a9fcd38377c5e6140e8a01
macndev <mndevilbiss@gmail.com>
2021-03-24 17:05:54 -0500

    adding CRV bar lengths and global coordinates to TCrvNumerology

--- geom/geom/TCrvNumerology.hh
+++ geom/geom/TCrvNumerology.hh
@@ -20,9 +20,43 @@ public:
     float   fBarLength;     //bar length in meters
   };
 
-  enum { kNSectors = 22 } ;
+  struct BarData_t {
+    int     fBarIndex;
+    int     fSector;
+    int     fSectorType;
+    float   fBarX;
+    float   fBarY;
+    float   fBarZ;
+    float   fWidthDir;
+    bool    localXY(float&X, float&Y){
+      if((fSector >= 0) && (fSector <= 6)){
+    	//right side sectors: 0-4, left side sectors: 5-6; dx/dz gives particle direction wrt beam
+    	X = fBarX;
+    	Y = fBarZ;
+	if((fSector >= 0) && (fSector <= 4)){
+	  X = -X; // flip right side to make slopes match left and top
+	}
+	return true;
+      }
+      else if((fSector >= 7) && (fSector <= 10)){
+    	//top side sectors: 7-10; dy/dz gives particle direction wrt beam
+    	X = fBarY;
+    	Y = fBarZ;
+	return true;
+      }
+      else{
+    	//nothing is needed if particle goes through U,D sectors
+        X = 0;
+	Y = 0;
+	return false;
+      }
+    }
+  };
+
+  enum { kNSectors = 22, kNBars = 5504 } ;
 
   SectorData_t fSector[kNSectors];
+  BarData_t    fBar[kNBars];
 
   static  TCrvNumerology* fgInstance;
 
@@ -46,7 +80,7 @@ public:
   int         NBarsPerLayer(int I) { return fSector[I].fNBarsPerLayer; }
   int         FirstIndex   (int I) { return fSector[I].fFirstIndex;    }
   float       BarLength    (int I) { return fSector[I].fBarLength;     }
-
+  bool        LocalBarXY   (int I, float&X, float&Y) { return fBar[I].localXY(X,Y); }
 //-----------------------------------------------------------------------------
 // other methods
 //-----------------------------------------------------------------------------
